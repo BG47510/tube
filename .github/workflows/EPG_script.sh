504bb4a3d61cc02493cabbe051a31897c8c72990
Zern <75311133+BG47510@users.noreply.github.com>
2026-02-06 14:45:35 +0100

    Update EPG_script.sh

--- .github/workflows/EPG_script.sh
+++ .github/workflows/EPG_script.sh
@@ -29,7 +29,7 @@ epg_count=0
 while IFS=, read -r epg; do
 	((epg_count++))
     extension="${epg##*.}"
-    temp="EPG_temp_${epg_count}"
+    temp="EPG_temp00${epg_count}"
 
     if [ "$extension" = "gz" ]; then
         log " │ Téléchargement et décompression: $epg"
